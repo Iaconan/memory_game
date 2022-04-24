7d6a9bdbdb82b7446bd4b0914f6ee4afc3c5f100
Vincenzo Iacona <vincenzoiacona@hotmail.com>
2022-04-24 15:11:51 +0200

    flip card and check match done

--- app.js
+++ app.js
@@ -55,16 +55,44 @@ cardArray.sort(() => 0.5 - Math.random())
 
 
 const gridDisplay = document.querySelector('#grid')
+let cardChosen = []
+let cardChosenIds = []
+const cardsWon =[]
 
 function createBoard() {
     for (let i = 0; i < cardArray.length; i++) {
         const card = document.createElement('img')
         card.setAttribute('src', 'images/blank.png')
         card.setAttribute('data-id', i)
+        card.addEventListener('click', flipCard)
         gridDisplay.appendChild(card)
         
     }
 }
 
 createBoard()
+
+function checkMatch() {
+    const cards = document.querySelectorAll('img')
+    if (cardChosen(0) === cardChosen[1]) {
+        alert('You found a match!')
+        cards[cardChosenIds[0]].setAttribute('src', 'images/white.png')
+        cards[cardChosenIds[1]].setAttribute('src', 'images/white.png')
+        cards[cardChosenIds[0]].removeEventListener('click', flipCard)
+        cards[cardChosenIds[1]].removeEventListener('click', flipCard)
+        cardsWon.push(cardChosen)
+    }
+    cardChosen = []
+    cardChosenIds = []
+}
+
+
+function flipCard() {
+    const cardId = this.getAttribute('data-id')
+    cardChosen.push(cardArray[cardId].name)
+    this.setAttribute('src', cardArray[cardId].img)
+    if (cardChosen.length === 2) {
+        setTimeout(checkMatch, 500)
+    }
+}
 
